1854f9593815326a6bb48830d71a55e3c183e865
kishoredr <kishore328@gmail.com>
2020-02-22 17:52:01 +0530

    updated Education details

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              
+              <li>Design by <a title="Kishore" href="https://github.com/kishoredr">Kishore D R</a></li>
            </ul>
 
         </div>
